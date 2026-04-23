385afc110927f7ca9ae83968e98f3aa97d39a49e
Eric Lu <eyl43@case.edu>
2026-04-23 19:25:15 -0400

    Fix base path to match repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/character-sheet/',
+  base: '/dnd5e_interactive_charsheet/',
   plugins: [react()],
 })
